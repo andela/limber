431bbec38233a3412f0e232027f007046ed95a52
Eric G <eric.gichuri@andela.com>
2015-11-10 17:46:16 +0300

    Set up app structure
    Initial commit with the structure agreed on for the application.

--- app/public/views.py
+++
@@ -1,9 +0,0 @@
-
-from django.shortcuts import render
-from django.http import HttpResponse
-#from django.template import RequestContext, loader
-
-
-# Create your views here.
-def index(request):
-	return render(request,"public/index.html")
