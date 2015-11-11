ecc548564f1b2b49ebaf05463d4258d03cddd6fc
Eric G <eric.gichuri@andela.com>
2015-11-11 15:25:47 +0300

    Delete commented line on wsgi.py

--- limber/wsgi.py
+++ limber/wsgi.py
@@ -14,6 +14,5 @@ from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "limber.settings")
 
-#application = get_wsgi_application()
 application = Cling(get_wsgi_application())
 
