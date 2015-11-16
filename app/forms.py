187de8cb37d852017461e1d1e4931085c077ec54
Mwaleh muturi <alex.mwaleh@andela.com>
2015-11-16 18:51:13 +0300

    Add Customerization to pages
    Added css to style.css
    Edited login.html
    Edited register.html

--- app/forms.py
+++ app/forms.py
@@ -2,8 +2,8 @@ from django import forms
 
 class LoginForm(forms.Form):
 	"""Fields for the login form"""
-	username = forms.CharField( max_length=100, widget=forms.TextInput(attrs={'class':'mdl-textfield__input'}))
-	password = forms.CharField(max_length=32, widget=forms.PasswordInput(attrs={'class':'mdl-textfield__input' }))
+	username = forms.CharField( max_length=100, widget=forms.TextInput(attrs={'class':'validate','type':'text', 'id' : 'icon_prefix'}))
+	password = forms.CharField(max_length=32, widget=forms.PasswordInput(attrs={'class':'mdl-textfield__input'}))
 
 class RegistrationForm(forms.Form):
 	"""Fields for the registration form"""
