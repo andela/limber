ca19bbc3a7f9cf80f1bb7085d459dcf647f29a71
Lewis Kabui <maina@Lewiss-MacBook-Pro.local>
2015-11-16 14:44:25 +0300

    Get DATABASE_URL variable from environment
    - Create .env file to store environment variables on virtualenv
    
    [Reference #107204654]

--- limber/settings.py
+++ limber/settings.py
@@ -103,9 +103,8 @@ USE_TZ = True
 
 # Parse database configuration from $DATABASE_URL
 
-# To connect to Heroku Postgres. Commented to facilitate model migrations - Lewis Kabui - 13/11/2015
 
-# DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
