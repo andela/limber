3bbddb82df3e33ed28f5620b8a69d4f25f993e86
Lewis Kabui <lewis.maina@andela.com>
2016-01-05 12:37:08 +0300

    [Finishes #109784762] Create tests for MemberViewSet's url
    - Created tests to simulate unauthenticated requests and successful post, get, put and delete requests to the "/api/member" url.

--- tests/test_limber_urls.py
+++ tests/test_limber_urls.py
@@ -20,10 +20,9 @@ class TestURLs(TestCase):
 		self.client = Client()
 
 		# a user to perform requests that require authentication
-		self.user = {
-			'username': fake.user_name(), 'email': fake.email(),
-			'password': fake.password()}
-		# store the user in the dattabase
+
+		self.user = {'username':fake.user_name(), 'email': fake.email(), 'password':fake.password()}
+		# create the user in the database
 		self.client.post('/api/user/', data=self.user)
 
 	def tearDown(self):
@@ -32,14 +31,18 @@ class TestURLs(TestCase):
 		del self.user
 
 	def test_api_url(self):
-		"""Test the main app urls."""
+		"""
+		Tests the '/api/' url.
+		"""
 		response = self.client.get('/api/')
 
 		self.assertEqual(response.status_code, 200)
 		self.assertEqual(response.status_text, 'OK')
 
 	def test_api_user_url(self):
-		"""Test urls related to a user."""
+		"""
+		Tests the '/api/user/' url.
+		"""
 		email = fake.email()
 		password = fake.password()
 		username = fake.user_name()
@@ -110,7 +113,9 @@ class TestURLs(TestCase):
 		self.assertFalse(user)
 
 	def test_api_org_url(self):
-		"""Test urls related to an organisation"""
+		"""
+		Tests the '/api/org' url.
+		"""
 		name = fake.name()
 		username = fake.user_name()
 
@@ -199,3 +204,107 @@ class TestURLs(TestCase):
 		org_db = User.objects.filter(username=username).first()
 		self.assertFalse(org_db)
 
+	def test_api_member_url(self):
+		"""
+		Tests the '/api/member'.
+		"""
+		# test unauthenticated GET request
+		response = self.client.get('/api/member/')
+		self.assertTrue('Authentication credentials were not provided' in response.data.get('detail'))
+		self.assertEqual(response.status_code, 403)
+		self.assertEqual(response.status_text, 'Forbidden')
+
+		# test unauthenticated POST request
+		response = self.client.post('/api/member/')
+		self.assertTrue('Authentication credentials were not provided' in response.data.get('detail'))
+		self.assertEqual(response.status_code, 403)
+		self.assertEqual(response.status_text, 'Forbidden')
+
+		# test unauthenticated DELETE request
+		response = self.client.delete('/api/member/')
+		self.assertTrue('Authentication credentials were not provided' in response.data.get('detail'))
+		self.assertEqual(response.status_code, 403)
+		self.assertEqual(response.status_text, 'Forbidden')
+
+		# test unauthenticated PUT request
+		response = self.client.put('/api/member/')
+		self.assertTrue('Authentication credentials were not provided' in response.data.get('detail'))
+		self.assertEqual(response.status_code, 403)
+		self.assertEqual(response.status_text, 'Forbidden')
+
+		# test a successful post request
+		# first login user
+		response = self.client.post('/api/api-auth/login/?next=/api/user/', data={'username': self.user.get('email'), 'password': self.user.get('password')})
+		# create an organisation to which members will be added
+		# the creator will be the default admin
+		org_name = fake.name()
+		org_username = fake.user_name()
+		response = self.client.post('/api/org/', data={'username': org_username, 'full_name': org_name})
+
+		# retrieve the org object from the DB using the id in the response
+		# then compare org.full_name with org_name
+		response = self.client.get('/api/member/')
+		org_id = response.data[0].get('org')
+		org_object = User.objects.get(pk=org_id)
+		self.assertEqual(org_name, org_object.full_name)
+		# also retrieve the user object from the info in the response
+		user_id = response.data[0].get('user')
+		default_admin = UserAuthentication.objects.get(pk=user_id)
+		self.assertEqual(self.user.get('email'), default_admin.email)
+		# ensure there is only one user in the org so far (the default_admin)
+		# and that he/she is admin (user_level 1)
+		self.assertEqual(len(response.data), 1)
+		self.assertEqual(response.data[0].get('user_level'), 1)
+		# make sure the status code is ok
+		self.assertTrue(response.status_code, 200)
+		self.assertTrue(response.status_text, 'OK')
+
+		# add a second member to organisation
+		# first create the user to be added
+		user2 = {
+			'email': fake.email(),
+			'username': fake.user_name(),
+			'password': fake.password()
+		}
+		response = self.client.post('/api/user/', data=user2)
+
+		# get the user's username from the response
+		# then use that username to fetch a user object from the DB
+		# this user object's id will be supplied as a foreign key to the user table when adding this member
+		response = self.client.get('/api/user/')
+		user2_username = response.data[1].get('username')
+		user_object_fk = User.objects.filter(username=user2_username)
+		user_object = UserAuthentication.objects.get(profile=user_object_fk)
+
+		# add user2 to the organisation as an admin (user_level 1)
+		response = self.client.post('/api/member/', data={'org': org_object.id, 'user': user_object.id, 'user_level': 1})
+		self.assertEqual(response.status_code, 201)
+		self.assertEqual(response.status_text, 'Created')
+		# View the new member.
+		response = self.client.get('/api/member/2/')
+		# ensure the user id in the response is the user id in the object
+		self.assertEqual(response.data.get('user'), user_object.id)
+		# ensure the organisation id in the response is the organisation id in the object
+		self.assertEqual(response.data.get('org'), org_object.id)
+		# ensure a user level of 1
+		self.assertEqual(response.data.get('user_level'), 1)
+
+		# test the PUT method to update member's user level in the org
+		json_data = json.dumps({'org': org_object.id, 'user': user_object.id, 'user_level': 3})
+		response = self.client.put('/api/member/2/', content_type='application/json', data=json_data)
+		self.assertEqual(response.data.get('user_level'), 3)
+		self.assertNotEqual(response.data.get('user_level'), 1)
+
+		# test the DELETE method
+		response = self.client.delete('/api/member/2/')
+		self.assertTrue('Member successfully removed' in response.data.get('status'))
+		# try to access after delete - expect 404
+		response = self.client.get('/api/member/2/')
+		self.assertTrue('Not found' in response.data.get('detail'))
+		self.assertEqual(response.status_code, 404)
+		self.assertEqual(response.status_text, 'Not Found')
+		# test delete on the only remaining admin in the org - this should not happen
+		response = self.client.delete('/api/member/1/')
+		self.assertEqual(response.status_code, 400)
+		self.assertEqual(response.status_text, 'Bad Request')
+		self.assertTrue('Member not removed' in response.data.get('status'))
