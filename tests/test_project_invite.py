073a55884f4103f3442bd56cce9e1bc8d880cef4
Lewis Kabui <lewis.maina@andela.com>
2016-01-13 21:29:19 +0300

    [Finishes #111395184] Add tests for unsuccessful post and put attempts

--- tests/test_project_invite.py
+++ tests/test_project_invite.py
@@ -165,6 +165,37 @@ class TestProjectInvite(TestCase):
 			in response.data.get('message')
 		)
 
+	def test_authenticated_unsuccessful_post(self):
+		""" Test an authenticated unsuccessful post request to
+			'/api/project-invites/' url.
+		"""
+		# user logs in
+		self.login_user()
+		# get the user object from the database
+		user_object = User.objects.get(username=self.user.get('username'))
+		# create a project
+		self.create_project(user_object)
+		# get the project object from the database
+		project_object = Project.objects.get(project_name=self.project_name)
+		# user sends post request to invite url
+		url = reverse('project-invites-list')
+		# generate a name instead of an email
+		email = fake.name()
+		response = self.client.post(
+			url,
+			data={
+				'email': email,
+				'project': project_object.project_id
+			}
+		)
+
+		self.assertEqual(response.status_code, 400)
+		self.assertEqual(response.status_text, 'Bad Request')
+		self.assertTrue(
+			'Invalid data'
+			in response.data.get('message')
+		)
+
 	def test_authenticated_put(self):
 		""" Test an authenticated put request to '/api/project-invites/<inv_code>/'
 			url.
@@ -211,3 +242,92 @@ class TestProjectInvite(TestCase):
 		self.assertEqual(response.status_code, 200)
 		self.assertEqual(response.status_text, 'OK')
 		self.assertEqual(new_status[0], response.data.get('accept'))
+
+	def test_authenticated_unsuccessful_put(self):
+		""" Test an authenticated put request to '/api/project-invites/<inv_code>/'
+			url.
+		"""
+		# user logs in
+		self.login_user()
+		# get the user object from the database
+		user_object = User.objects.get(username=self.user.get('username'))
+		# create a project
+		self.create_project(user_object)
+		# get the project object from the database
+		project_object = Project.objects.get(project_name=self.project_name)
+		# user sends post request to invite url (and create an invite record
+		# on the database)
+		url = reverse('project-invites-list')
+		email = fake.email()
+		response = self.client.post(
+			url,
+			data={
+				'email': email,
+				'project': project_object.project_id
+			}
+		)
+		# get the projectinvite object from the database
+		project_invite_obj = ProjectInvite.objects.get(
+			project=project_object.project_id
+		)
+		# create the '/api/project-invites/<inv_code>/' url
+		url += project_invite_obj.invite_code + '/'
+		# create put request to change accept status to invalid option
+		new_status = fake.email()
+		json_data = json.dumps(
+			{
+				'accept': new_status,
+				'email': email,
+				'project': project_object.project_id
+			}
+		)
+		response = self.client.put(
+			url,
+			data=json_data,
+			content_type='application/json'
+		)
+		# import ipdb; ipdb.set_trace()
+		self.assertEqual(response.status_code, 400)
+		self.assertEqual(response.status_text, 'Bad Request')
+		self.assertTrue(
+			'is not a valid choice' in
+			response.data.get('accept')[0]
+		)
+
+	def test_authenticated_delete(self):
+		""" Test an authenticated delete request
+			to '/api/project-invites/<inv_code>/' url.
+		"""
+		# user logs in
+		self.login_user()
+		# get the user object from the database
+		user_object = User.objects.get(username=self.user.get('username'))
+		# create a project
+		self.create_project(user_object)
+		# get the project object from the database
+		project_object = Project.objects.get(project_name=self.project_name)
+		# user sends post request to invite url (and create an invite record
+		# on the database)
+		url = reverse('project-invites-list')
+		email = fake.email()
+		response = self.client.post(
+			url,
+			data={
+				'email': email,
+				'project': project_object.project_id
+			}
+		)
+		# get the projectinvite object from the database
+		project_invite_obj = ProjectInvite.objects.get(
+			project=project_object.project_id
+		)
+		# create the '/api/project-invites/<inv_code>/' url
+		url += project_invite_obj.invite_code + '/'
+		# send the delete  request
+		response = self.client.delete(url)
+		self.assertEqual(response.status_code, 204)
+		self.assertEqual(response.status_text, 'No Content')
+		# expect 404 not found after delete
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, 404)
+		self.assertEqual(response.status_text, 'Not Found')
