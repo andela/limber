8f3b67d64326153e35fac16e8745dbc1566837f8
Collin Mutembei <mutembei.collin@gmail.com>
2016-01-18 16:32:57 +0300

    [Chore #111455374] Implement adding JWT in header
    - Adds token to Authorization header for request after
      successful login. [WIP]

--- public/static/scripts/app.js
+++ public/static/scripts/app.js
@@ -1,1 +1,20 @@
-angular.module('limberApp', ['ngResource', 'ngCookies']);
+angular.module('limberApp', ['ngResource', 'ngCookies'])
+.factory('sessionInjector', function($cookies) {
+    console.log($cookies.get("token"));
+    var sessionInjector = {
+        request: function(config) {
+            var token = $cookies.get("token");
+            console.log(token);
+            config.headers['Authorization'] = "JWT " + token;
+            return config;
+        }
+    };
+    return sessionInjector;
+})
+.config(function ($httpProvider) {
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+    $httpProvider.interceptors.push('sessionInjector');
+    // $httpProvider.defaults.headers.common['Authorization'] = 'divnDvjkSFJCBisdzfladsiocguhsfjcnfkcsahd';
+
+})
--- public/static/scripts/controllers/signup.js
+++ public/static/scripts/controllers/signup.js
@@ -1,41 +1,30 @@
 angular.module('limberApp').
-    // config(['$httpProvider', function($httpProvider){
-    //     // django and angular both support csrf tokens. This tells
-    //     // angular which cookie to add to what header.
-    //     $httpProvider.defaults.xsrfCookieName = 'csrftoken';
-    //     $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
-    //     // console.log($scope)
-    // }]).
     factory('api', function($resource){
-        // function add_auth_header(data, headersGetter){
-        //     var headers = headersGetter();
-        //     console.log(data);
-        //     headers['Authorization'] = ('JWT ' + $scope.token);
-        // }
-        // defining the endpoints. Note we escape url trailing dashes: Angular
-        // strips unescaped trailing slashes. Problem as Django redirects urls
-        // not ending in slashes to url that ends in slash for SEO reasons, unless
-        // we tell Django not to [3]. This is a problem as the POST data cannot
-        // be sent with the redirect. So we want Angular to not strip the slashes!
         return {
-            auth: $resource('/api/api-token-auth\\/', {}, {
+            auth: $resource('/api/api-token-auth/', {}, {
                 login: {method: 'POST'},
                 logout: {method: 'DELETE'}
+            },{
+                stripTrailingSlashes: false
             }),
-            users: $resource('/api/user\\/', {}, {
+            users: $resource('/api/user/', {}, {
                 create: {method: 'POST'}
+            },{
+                stripTrailingSlashes: false
             })
         };
     }).
     controller('authController', function($scope, api, $cookies, $location, $window) {
 
+        $scope.confirmmessage = ""
+
         $scope.login = function(){
-            // dt = {email: $scope.username, password: $scope.password};
             api.auth.login($scope.user).
                 $promise.
                     then(function(data){
                         $cookies.put('token', data.token);
-                        // $window.location.href = '/api/user'
+                        $window.location.href = '/api/user'
+
                     }).
                     catch(function(data){
                         console.log(data);
@@ -50,15 +39,16 @@ angular.module('limberApp').
             });
         };
         $scope.register = function(){
-            // dt = {email: $scope.email, username: $scope.username, password: $scope.password};
             api.users.create($scope.signup).
                 $promise.
-                    then(function(data){
-                        console.log(data);
-                        // $window.location.href = '/api'
+                    then(function(result){
+                        console.log(result);
+                        $scope.confirmmessage = "Registration complete. Please confirm your email address before logging in."
+                        $scope.signup = {}
+                        $window.location.href = '/api'
                     }).
-                    catch(function(data){
-                        console.log(data);
+                    catch(function(response){
+                        console.log(response);
                     });
             };
     });
