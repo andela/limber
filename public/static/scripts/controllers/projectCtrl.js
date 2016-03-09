5f1b6c35781d922928dff193f499421c3f25f037
Lewis Kabui <lewis.maina@andela.com>
2016-03-09 15:17:54 +0300

    [Feature #112311313] Create logic to add organisational projects
    - Define method createOrganisationalProject
    - Abstract logic to fetch organisational projects to a method so as to
    call it and refresh organisational projects when a new project is
    created

--- public/static/scripts/controllers/projectCtrl.js
+++ public/static/scripts/controllers/projectCtrl.js
@@ -3,20 +3,44 @@ app.controller('ProjectCtrl', function($scope, $cookies, mainService){
 
     var loadPersonalProjects = function () {
     	$scope.personal_projects = mainService.personal.getProjects();
-    }
+    };
     loadPersonalProjects();
-    $scope.org_projects = mainService.org.getProjects();
+    var loadOrgProjects = function () {
+    	$scope.org_projects = mainService.org.getProjects();
+    };
+    loadOrgProjects();
     $scope.other_projects = mainService.other.getProjects();
     $scope.personal = {};
+    $scope.organisational = {};
 
     $scope.createPersonalProject = function () {
-
-    	console.log($scope.personal);
-    	mainService.Projects.createPersonalProject($scope.personal).$promise.then(
+    	mainService.Projects.createProject($scope.personal).$promise.then(
     		function (response) {
     			loadPersonalProjects()
     			$scope.personal.project_name = '';
     			$scope.personal.project_desc = '';
+    		},
+    		function (error) {
+    			console.log(error);
+    		}
+    	);
+    };
+
+    $scope.createOrganisationalProject = function () {
+    	mainService.Projects.createProject($scope.organisational).$promise.then(
+    		function (response) {
+    			loadOrgProjects();
+    		},
+    		function (error) {
+    			console.log(error);
+    		}
+    	);
+    };
+
+    $scope.getAssociatedOrgs = function () {
+    	mainService.OrgAssociations.getAll().$promise.then(
+    		function (response) {
+    			$scope.org_associations = response;
     		},
     		function (error) {
     			console.log(error);
