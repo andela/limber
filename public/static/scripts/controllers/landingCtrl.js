b135317a42cf3e8945795e7f59f025283e405190
Collin Mutembei <mutembei.collin@gmail.com>
2016-03-23 11:40:06 +0300

    [Fix #115229619] Exclude some typed text
    - remove your girlfriend from the picture
    - fix typo

--- public/static/scripts/controllers/landingCtrl.js
+++ public/static/scripts/controllers/landingCtrl.js
@@ -1,6 +1,6 @@
 app.controller('LandingController', function($scope){
     $(".animation").typed({
-        strings: ["agile software development", "taking your girlfriend to shop", "better project management."],
+        strings: ["agile software development", "faster project delivery",  "effective project planning", "better project management."],
         typeSpeed: 70,
         backDelay: 70,
         loop: true,
@@ -8,4 +8,3 @@ app.controller('LandingController', function($scope){
      });
      $('.slider').slider({height: 120, indicators: false, interval : 2000});
 });
-
